fbab16ab9fb3f60625424bb2e8d75d90b1ef7afa
Igor Pereira <pereira.t.igor@gmail.com>
2023-10-22 18:23:49 -0300

    Added update and delete functions

--- firstAPI/src/controllers/UserController.js
+++ firstAPI/src/controllers/UserController.js
@@ -1,4 +1,5 @@
-const users = require('../mocks/users');
+const { after } = require('node:test');
+let users = require('../mocks/users');
 
 module.exports = {
     listUsers(request, response){
@@ -39,4 +40,33 @@ module.exports = {
         users.push(newUser);
         response.send(200, newUser);
     },
+    updateUser(request, response){
+        let { id } = request.params;
+        const { name } = request.body;
+
+        id = Number(id);
+
+        const userExists = users.find((user) => user.id === id);
+
+        if(!userExists){
+            return response.send(400, { error: "User not found"});
+        }
+
+        users = users.map((user) => {
+            if(user.id === id){
+                return {
+                    ...user,
+                    name,
+                };
+            }
+            return user;
+        });
+        response.send(200, {id, name});
+    },
+    deleteUser(request, response){
+        let { id } = request.params;
+        id = Number(id);
+        users = users.filter((user) => user.id != id);
+        response.send(200, {deleted: true});
+    }
 };
